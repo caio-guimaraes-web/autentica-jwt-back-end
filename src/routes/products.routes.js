f7ec8ef5ce456cc60c59486a6c7df56a801b0793
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-18 15:36:30 -0300

    applying authorization on a single route

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,14 +1,19 @@
-const { Router } = require("express");
-const ProductsController = require("../controllers/ProductsController");
-const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const { Router } = require("express")
+const ProductsController = require("../controllers/ProductsController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
 
-const productsRoutes = Router();
+const productsRoutes = Router()
 
-const productsController = new ProductsController();
+const productsController = new ProductsController()
 
-productsRoutes.use(ensureAuthenticated);
+productsRoutes.use(ensureAuthenticated)
 
-productsRoutes.get("/", productsController.index);
-productsRoutes.post("/", productsController.create);
+productsRoutes.get("/", productsController.index)
+productsRoutes.post(
+  "/",
+  verifyUserAuthorization("admin"),
+  productsController.create
+)
 
-module.exports = productsRoutes;
+module.exports = productsRoutes
