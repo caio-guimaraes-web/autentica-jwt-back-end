0d8151008e2d1a68db494ebe0a3d4d6450e2c426
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-18 15:27:01 -0300

    applying authorization on a set of routes

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -1,13 +1,15 @@
-const { Router } = require("express");
-const SalesController = require("../controllers/SalesController");
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const { Router } = require("express")
+const SalesController = require("../controllers/SalesController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
 
-const salesRoutes = Router();
+const salesRoutes = Router()
 
-const salesController = new SalesController();
+const salesController = new SalesController()
 
-salesRoutes.use(ensureAuthenticated);
+salesRoutes.use(ensureAuthenticated)
+salesRoutes.use(verifyUserAuthorization("admin"))
 
-salesRoutes.get("/", salesController.index);
+salesRoutes.get("/", salesController.index)
 
-module.exports = salesRoutes;
+module.exports = salesRoutes
